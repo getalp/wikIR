3a26775ebce86878373c74a9492385f4b1b2de15
jibrilfrej <jibrilfrej@yahoo.fr>
2019-11-17 21:22:20 +0100

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -6,8 +6,8 @@ python wikiextractor/WikiExtractor.py enwiki-20191101-pages-articles-multistream
 
 rm enwiki-20191101-pages-articles-multistream.xml
 
-python build_wikIR.py -in enwiki.json -o dev_wikIR -maxd 100000 -val 100 -test 100 -title -first -skip -low -bm25
+python build_wikIR.py -in enwiki.json -o $1/dev_wikIR -maxd 100000 -val 100 -test 100 -title -first -skip -low -bm25
 
-python build_wikIR.py -in enwiki.json -o wikIR -title -first -skip -low -bm25
+python build_wikIR.py -in enwiki.json -o $1/wikIR -title -first -skip -low -bm25
 
 rm enwiki.json
